e68bba1faf5d87df956dc6bed295cd43a9bc48f9
Dark-Robin <sihileld23@gmail.com>
2024-12-13 16:15:31 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -67,7 +67,7 @@ router.get('/', async (req, res) => {
 
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
 
-                        const sid = `*ROBIN [The powerful WA BOT]*\n\n👉 *${string_session}* 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1Y*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
+                        const sid = `*ROBIN [The powerful WA BOT]*\n\n👉 *${string_session}* 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
                         const mg = `🛑 *Do not share this code to anyone* 🛑`;
                         const dt = await PrabathPairWeb.sendMessage(user_jid, { image: { url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20system.jpg"},caption:sid});
                         const msg = await PrabathPairWeb.sendMessage(user_jid, {text: string_session});
